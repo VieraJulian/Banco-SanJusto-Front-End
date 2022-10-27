7fb14b43419d23c47c774ad79b6aa98303ed3d13
VieraJulian <julianviera2019@hotmail.com>
2022-10-27 17:57:20 -0300

    Arreglos en el css de login, faltan arreglos en desktop

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,8 +11,12 @@ function Login() {
             </section>
             <form action="/user/login" method="post" className="form-login">
                 <p className="first-p">Ingresá tus datos para operar</p>
-                <input type="text" placeholder="Tu número de tarjeta" /><i class="fa-solid fa-eye"></i>
-                <input type="text" placeholder="Tu pin" /><i class="fa-solid fa-eye"></i>
+                <div className="input-container">
+                    <input type="text" placeholder="Tu número de tarjeta" /><i class="fa-solid fa-eye"></i>
+                </div>
+                <div className="input-container">
+                    <input type="text" placeholder="Tu pin" /><i class="fa-solid fa-eye"></i>
+                </div>
                 <p className="second-p">No compartas los códigos que te enviamos por SMS o email, el Token de seguridad, los datos de tu tarjeta de coordenadas ni tus claves, <a href="#">Conocé más consejos de seguridad</a></p>
                 <button className="button-login" type="submit">Ingresar</button>
                 <p className="login-p">Si no tenés u olvidaste tu clave y/o usuario</p>
